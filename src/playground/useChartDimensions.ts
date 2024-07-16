68eb05f7a3bd66cbad4f6e842eb5e3491aecd30a
jlsuh <joelleonardosuh@gmail.com>
2024-07-15 23:57:16 -0300

    update

--- src/playground/useChartDimensions.ts
+++ src/playground/useChartDimensions.ts
@@ -2,72 +2,51 @@ import { useEffect, useRef, useState } from 'react';
 
 export type Dimensions = {
   height: number;
-  marginBottom?: number;
-  marginLeft?: number;
-  marginRight?: number;
-  marginTop?: number;
+  marginBottom: number;
+  marginLeft: number;
+  marginRight: number;
+  marginTop: number;
   width: number;
 };
 
-function combineChartDimensions(dimensions: Dimensions) {
-  const parsedDimensions = {
-    marginBottom: 30,
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 30,
-    ...dimensions,
-  };
-
+function composeChartDimensions(dimensions: Dimensions) {
   return {
-    ...parsedDimensions,
+    ...dimensions,
     boundedHeight: Math.max(
-      parsedDimensions.height -
-        parsedDimensions.marginTop -
-        parsedDimensions.marginBottom,
+      dimensions.height - dimensions.marginTop - dimensions.marginBottom,
       0,
     ),
     boundedWidth: Math.max(
-      parsedDimensions.width -
-        parsedDimensions.marginLeft -
-        parsedDimensions.marginRight,
+      dimensions.width - dimensions.marginLeft - dimensions.marginRight,
       0,
     ),
   };
 }
 
 function useChartDimensions(dimensions: Dimensions) {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [currentHeight, setCurrentHeight] = useState(0);
+  const [currentWidth, setCurrentWidth] = useState(0);
   const ref = useRef(null);
 
-  const combinedDimensions = combineChartDimensions(dimensions);
+  const chartDimensions = composeChartDimensions(dimensions);
 
   useEffect(() => {
-    if (combinedDimensions.width && combinedDimensions.height) return;
     const element = ref.current ?? new HTMLDivElement();
     const resizeObserver = new ResizeObserver((entries) => {
-      if (!Array.isArray(entries)) {
-        return;
-      }
-      if (!entries.length) {
-        return;
-      }
-      const entry = entries[0];
-      if (width !== entry.contentRect.width) {
-        setWidth(entry.contentRect.width);
-      }
-      if (height !== entry.contentRect.height) {
-        setHeight(entry.contentRect.height);
-      }
+      const { contentRect } = entries[0];
+      if (currentHeight !== contentRect.height)
+        setCurrentHeight(contentRect.height);
+      if (currentWidth !== contentRect.width)
+        setCurrentWidth(contentRect.width);
     });
     resizeObserver.observe(element);
     return () => resizeObserver.unobserve(element);
-  }, [combinedDimensions.height, combinedDimensions.width, height, width]);
+  }, [currentHeight, currentWidth]);
 
-  const newDimensions = combineChartDimensions({
-    ...combinedDimensions,
-    width: combinedDimensions.width || width,
-    height: combinedDimensions.height || height,
+  const newDimensions = composeChartDimensions({
+    ...chartDimensions,
+    width: chartDimensions.width || currentWidth,
+    height: chartDimensions.height || currentHeight,
   });
 
   return { newDimensions, ref };
