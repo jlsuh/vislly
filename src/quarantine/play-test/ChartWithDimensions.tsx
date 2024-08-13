ba720eb9d641dce2ab833add7528de9a2e82a7d6
jlsuh <joelleonardosuh@gmail.com>
2024-08-13 02:29:15 -0300

    update

--- src/quarantine/play-test/ChartWithDimensions.tsx
+++ src/quarantine/play-test/ChartWithDimensions.tsx
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
+import useChartDimensions from '../useChartDimensions';
 import Axis from './Axis';
-import useChartDimensions from './useChartDimensions';
 
 const chartSettings = {
   boundedHeight: 0,
