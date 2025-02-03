d42090ca0805bec494e786a4afd43e5ebc02af5c
jlsuh <joelleonardosuh@gmail.com>
2025-02-03 00:28:00 -0300

    styles: new themes without tailwind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,26 +3,18 @@ import type { ChangeEvent, JSX } from 'react';
 
 const changeTheme = (e: ChangeEvent<HTMLSelectElement>) => {
   const { value } = e.target;
-  if (value !== 'light') {
-    document.documentElement.classList.remove('light');
-    return;
-  }
-  document.documentElement.classList.add('light');
+  document.documentElement.dataset.theme = value;
+  localStorage.setItem('theme', value);
 };
 
 function App(): JSX.Element {
   return (
     <>
-      <select
-        className="border-info text-info text-center p-5 rounded-5 text-sm bg-primary"
-        id="theme"
-        onChange={changeTheme}
-        title="Select theme"
-      >
+      <select id="theme" onChange={changeTheme} title="Select theme">
         <option value="dark">Dark</option>
         <option value="light">Light</option>
       </select>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Hello world!</h1>
       <BrownianMotion />
     </>
   );
