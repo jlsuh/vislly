b5eb99574b2d7d26fb852e4392936aa59c6538f6
jlsuh <joelleonardosuh@gmail.com>
2025-02-08 05:30:45 -0300

    refactor: rename div entrypoint id

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './app/styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('genesis') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
