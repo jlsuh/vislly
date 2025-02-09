5cfc0ec4bdc837063c8cd7b30c6ed15d8cea2913
jlsuh <joelleonardosuh@gmail.com>
2025-02-09 03:23:20 -0300

    chore(App.tsx): rename to 'AppWithProviders'

---
+++ src/AppWithProviders.tsx
@@ -0,0 +1,34 @@
+import BrownianMotion from '@/entities/brownian-motion/ui/BrownianMotion';
+import { type ComponentType, type JSX, use } from 'react';
+import ThemeContext from './app/providers/ThemeContext';
+import withProviders from './app/providers/withProviders';
+import isThemeValue from './shared/lib/theme/isThemeValue';
+import { Theme } from './shared/lib/theme/theme';
+
+function App(): JSX.Element {
+  const { changeTheme, theme } = use(ThemeContext);
+
+  return (
+    <>
+      <select
+        onChange={(e) => {
+          const value = e.target.value;
+          if (isThemeValue(value)) changeTheme(value);
+        }}
+        title="Select Theme"
+        value={theme}
+        id="theme-select"
+      >
+        <option value={Theme.Auto}>System Default</option>
+        <option value={Theme.Dark}>Dark</option>
+        <option value={Theme.Light}>Light</option>
+      </select>
+      <h1>Hello world!</h1>
+      <BrownianMotion />
+    </>
+  );
+}
+
+const AppWithProviders: ComponentType = withProviders(App);
+
+export default AppWithProviders;
