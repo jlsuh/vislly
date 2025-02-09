06dbe6c5983c29417344ff019074aecb1370996a
jlsuh <joelleonardosuh@gmail.com>
2025-02-08 23:25:40 -0300

    chore: rename to entry

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app/styles/globals.css';
+import './app/styles/entry.css';
 
 ReactDOM.createRoot(document.getElementById('genesis') as HTMLElement).render(
   <React.StrictMode>
