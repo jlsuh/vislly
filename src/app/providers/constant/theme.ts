42dc63f04837743d42557299f58a0fefeea77683
jlsuh <joelleonardosuh@gmail.com>
2025-02-11 01:49:03 -0300

    types(theme): rename 'ThemeObject' -> 'ThemeType'

--- src/app/providers/constant/theme.ts
+++ src/app/providers/constant/theme.ts
@@ -4,11 +4,27 @@ const DARK_LIGHT = 'dark light';
 const ONLY_DARK = 'only dark';
 const ONLY_LIGHT = 'only light';
 
+type ThemeValue = typeof DARK_LIGHT | typeof ONLY_DARK | typeof ONLY_LIGHT;
+
 const DARK_LIGHT_LABEL = 'System';
 const ONLY_DARK_LABEL = 'Always Dark';
 const ONLY_LIGHT_LABEL = 'Always Light';
 
-const Theme: ReadonlyDeep<Record<ThemeValue, ThemeObject>> = {
+type ThemeLabel =
+  | typeof DARK_LIGHT_LABEL
+  | typeof ONLY_DARK_LABEL
+  | typeof ONLY_LIGHT_LABEL;
+
+interface ThemeType {
+  label: ThemeLabel;
+  shouldTriggerViewTransition: (
+    targetTheme: ThemeValue,
+    userPrefersDarkTheme: boolean,
+  ) => boolean;
+  value: ThemeValue;
+}
+
+const Theme: ReadonlyDeep<Record<ThemeValue, ThemeType>> = {
   [DARK_LIGHT]: {
     label: DARK_LIGHT_LABEL,
     shouldTriggerViewTransition(
@@ -44,19 +60,5 @@ const Theme: ReadonlyDeep<Record<ThemeValue, ThemeObject>> = {
 };
 
 const FALLBACK_THEME_VALUE: ThemeValue = ONLY_DARK;
-
-type ThemeValue = typeof DARK_LIGHT | typeof ONLY_DARK | typeof ONLY_LIGHT;
-type ThemeLabel =
-  | typeof DARK_LIGHT_LABEL
-  | typeof ONLY_DARK_LABEL
-  | typeof ONLY_LIGHT_LABEL;
-type ThemeObject = {
-  label: ThemeLabel;
-  shouldTriggerViewTransition: (
-    targetTheme: ThemeValue,
-    userPrefersDarkTheme: boolean,
-  ) => boolean;
-  value: ThemeValue;
-};
 
 export { FALLBACK_THEME_VALUE, Theme, type ThemeValue };
