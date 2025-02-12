24d08f10889fd54c72558ab3502cc80977fade54
jlsuh <joelleonardosuh@gmail.com>
2025-02-12 00:32:57 -0300

    refactor: pass down handleOnChangeTheme handler & extract theme select

--- src/AppWithProviders.tsx
+++ src/AppWithProviders.tsx
@@ -1,28 +1,24 @@
-import { Theme } from '@/app/providers/constant/theme';
 import ThemeContext from '@/app/providers/ui/ThemeContext';
 import withProviders from '@/app/providers/ui/withProviders';
 import BrownianMotion from '@/entities/brownian-motion/ui/BrownianMotion';
-import Button from '@/shared/ui/Button/Button';
+import Header from '@/shared/ui/Header/Header';
+import ThemeSelect from '@/shared/ui/ThemeSelect/ThemeSelect';
 import { type ComponentType, type JSX, use } from 'react';
 
 function App(): JSX.Element {
   const { changeTheme, currentThemeValue } = use(ThemeContext);
 
+  const handleOnChangeTheme = (value: string) => {
+    changeTheme(value);
+  };
+
   return (
     <>
-      <select
-        onChange={(e) => changeTheme(e.target.value)}
-        title="Select Theme"
-        value={currentThemeValue}
-        id="theme-select"
-      >
-        {Object.values(Theme).map(({ label, value }) => (
-          <option key={value} value={value}>
-            {label}
-          </option>
-        ))}
-      </select>
-      <Button />
+      <Header />
+      <ThemeSelect
+        changeTheme={handleOnChangeTheme}
+        currentThemeValue={currentThemeValue}
+      />
       <h1>Hello world!</h1>
       <BrownianMotion />
     </>
