22bb0311fba428e869e98a023a7e4744b29aef68
jlsuh <joelleonardosuh@gmail.com>
2025-02-18 01:58:31 -0300

    refactor(withProviders): delete providers.ts and withProvider.tsx as withProviders is the only dependant module & use proper 'Wrap' and 'Wrappable' semantics for provider and wrapped components

--- src/app/providers/withProviders.tsx
+++ src/app/providers/withProviders.tsx
@@ -1,11 +1,44 @@
-import type { ComponentType } from 'react';
-import PROVIDERS from './providers';
-import withProvider from './withProvider';
+import type { ComponentType, PropsWithChildren } from 'react';
+import { BrowserRouter } from 'react-router';
+import type { ReadonlyDeep } from 'type-fest';
+import ThemeProvider from './ThemeProvider';
+
+type Provider = ComponentType<PropsWithChildren>;
+
+interface Wrap {
+  Wrappable: ComponentType;
+  Wrapper: Provider;
+}
+
+const PROVIDERS: ReadonlyDeep<Array<Provider>> = [BrowserRouter, ThemeProvider];
+
+/**
+ * @param Wrappable.displayName - Used for debugging purposes in user-defined components.
+ * @param Wrappable.name - Used for debugging purposes in HTML elements.
+ * @param Wrapper.displayName - Used for debugging purposes in user-defined components.
+ * @param Wrapper.name - Used for debugging purposes in HTML elements.
+ */
+function composeDisplayName({ Wrappable, Wrapper }: Wrap): string {
+  const wrappableName = Wrappable.displayName || Wrappable.name || 'Component';
+  const wrapperName = Wrapper.displayName || Wrapper.name || 'Provider';
+  return `with${wrapperName}(${wrappableName})`;
+}
+
+function withProvider({ Wrappable, Wrapper }: Wrap): ComponentType {
+  function WithProvider() {
+    return (
+      <Wrapper>
+        <Wrappable />
+      </Wrapper>
+    );
+  }
+  WithProvider.displayName = composeDisplayName({ Wrappable, Wrapper });
+  return WithProvider;
+}
 
 function withProviders(Component: ComponentType): ComponentType {
   return PROVIDERS.reduceRight(
-    (ComponentWrappedWithProviders, Provider) =>
-      withProvider(Provider, ComponentWrappedWithProviders),
+    (Wrappable, Wrapper) => withProvider({ Wrappable, Wrapper }),
     Component,
   );
 }
