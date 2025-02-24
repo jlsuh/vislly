aec2a2d1de49ad5affa81825c7a25ce2218a5671
jlsuh <joelleonardosuh@gmail.com>
2025-02-23 23:08:15 -0300

    chore: visualize texts

--- src/AppWithProviders.tsx
+++ src/AppWithProviders.tsx
@@ -1,13 +1,44 @@
-import BrownianMotion from '@/entities/brownian-motion/ui/BrownianMotion';
 import type { ComponentType, JSX } from 'react';
 import Header from './app/layout/Header.tsx';
 import withProviders from './app/providers/withProviders.tsx';
+import BrownianMotion from './entities/brownian-motion/ui/BrownianMotion.tsx';
 
 function App(): JSX.Element {
   return (
     <>
       <Header />
       <h1>Hello world!</h1>
+      <h1
+        style={{
+          fontFamily: 'Mona Sans',
+        }}
+      >
+        Hello world!
+      </h1>
+      <h1>g f</h1>
+      <p
+        style={{
+          fontFamily: 'MonaSansFallback',
+          fontStyle: 'italic',
+          fontWeight: 'bold',
+        }}
+      >
+        Hello world!
+      </p>
+      <br />
+      <b
+        style={{
+          fontFamily: 'MonaspaceNeonFallback',
+        }}
+      >
+        Hello world!
+      </b>
+      <br />
+      <i>Hello world!</i>
+      <br />
+      <b>Hello world!</b>
+      <br />
+      <p>Hello world!</p>
       <BrownianMotion />
     </>
   );
