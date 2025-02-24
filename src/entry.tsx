b5b1132c07127798ad68c3e8d149d1ab8bd9fa21
jlsuh <joelleonardosuh@gmail.com>
2025-02-23 23:09:28 -0300

    fix(entry.tsx): battle against FOUT/FOIT via document.fonts.ready

--- src/entry.tsx
+++ src/entry.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import AppWithProviders from './AppWithProviders.tsx';
 import './app/styles/entry.css';
 
-ReactDOM.createRoot(document.getElementById('genesis') as HTMLElement).render(
-  <React.StrictMode>
-    <AppWithProviders />
-  </React.StrictMode>,
-);
+document.fonts.ready.then(() => {
+  ReactDOM.createRoot(document.getElementById('genesis') as HTMLElement).render(
+    <React.StrictMode>
+      <AppWithProviders />
+    </React.StrictMode>,
+  );
+});
