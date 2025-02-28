7fe6f9bb398c133c30b066d58629ade633b3d252
jlsuh <joelleonardosuh@gmail.com>
2025-02-28 20:30:49 -0300

    feat: brownian motion page

---
+++ app/brownian-motion/page.tsx
@@ -0,0 +1,63 @@
+import BrownianMotion from '@/entities/brownian-motion/ui/BrownianMotion';
+import type { JSX } from 'react';
+
+export default function Page(): JSX.Element {
+  return (
+    <>
+      <h1>Brownian Motion</h1>
+      <BrownianMotion />
+      <p>Just something to try</p>
+      <p>
+        Consequat et sit enim sint Lorem adipisicing exercitation est in ipsum.
+        Sit eiusmod Lorem et qui cupidatat culpa sint proident amet labore
+        fugiat qui nostrud. Sint occaecat duis sint mollit duis. Id irure
+        aliquip nostrud ad elit tempor proident eu esse. Proident esse nulla non
+        nisi veniam esse nostrud. Voluptate deserunt aliquip commodo enim fugiat
+        sint mollit nostrud nisi in ullamco. Laboris excepteur ipsum duis
+        deserunt reprehenderit qui cupidatat sit Lorem anim. Ad esse nisi
+        laborum Lorem. Do culpa voluptate nostrud non cupidatat exercitation
+        sunt dolore ea dolor reprehenderit id. Commodo nulla laborum aliquip
+        labore cupidatat est. Minim veniam aliqua nulla adipisicing in do mollit
+        dolor Lorem. Ut fugiat et labore veniam ipsum mollit ullamco excepteur
+        nisi veniam laborum exercitation. Pariatur in dolor quis elit id
+        consectetur cupidatat ullamco. Ut id cillum ex laborum minim cillum
+        quis. Lorem occaecat non occaecat deserunt culpa tempor laborum ad elit
+        irure id ullamco sit. Quis cupidatat velit in pariatur qui laboris.
+        Lorem deserunt ut laboris eu. Cupidatat pariatur tempor tempor ea aliqua
+        nostrud incididunt eiusmod. Quis sit labore deserunt minim velit. Sint
+        do dolor sit velit tempor pariatur ullamco. Minim ipsum duis enim culpa.
+        Veniam et reprehenderit eu magna. In consectetur laborum amet amet
+        fugiat. Commodo Lorem duis consectetur aliqua ex. Laborum deserunt in
+        Lorem dolor. Laborum fugiat ad occaecat commodo. Ad exercitation et quis
+        minim proident amet quis sit eiusmod non magna elit do. Nisi officia
+        dolor dolore eiusmod dolor laboris nisi consectetur do cillum cillum
+        exercitation est. Dolore minim voluptate enim nulla dolore adipisicing
+        eiusmod labore ex ipsum aliqua Lorem qui elit. Id incididunt sunt ipsum
+        incididunt minim et velit. Aliqua elit voluptate enim sunt commodo.
+        Exercitation sit do adipisicing Lorem in occaecat. Commodo nisi esse qui
+        elit aliqua voluptate anim elit enim pariatur. Qui aliqua excepteur est
+        deserunt elit. Ad sint et nulla est proident eiusmod velit duis esse
+        voluptate amet elit. Aliqua esse exercitation incididunt veniam veniam
+        incididunt ad irure voluptate consequat. Nisi ad pariatur ullamco nulla
+        eiusmod magna ex cillum dolor ex sint labore consectetur. Tempor mollit
+        amet proident nostrud et do. Adipisicing est mollit voluptate consequat
+        fugiat labore anim amet ut dolore quis et sint. Deserunt id aliqua culpa
+        enim aute proident. Officia ipsum enim minim nostrud veniam elit culpa
+        cillum excepteur velit. Ea laborum officia laborum aliquip ea
+        consectetur esse anim elit fugiat ipsum incididunt excepteur est. Aute
+        aliquip incididunt fugiat ad irure in pariatur. Est reprehenderit
+        pariatur in ullamco. Et eiusmod adipisicing tempor sit id nulla officia
+        aute officia. Magna eu laborum culpa duis ullamco incididunt culpa
+        commodo veniam mollit. Excepteur sit voluptate esse deserunt et nulla et
+        dolore sint et. Sit magna ut amet ipsum pariatur laboris labore culpa
+        dolor nisi exercitation. Sint do veniam proident incididunt sit labore.
+        Nulla cillum eu tempor occaecat mollit nostrud eiusmod do cillum dolore
+        cupidatat officia. Minim veniam dolor et occaecat. Incididunt amet irure
+        tempor amet do excepteur culpa non adipisicing. Voluptate deserunt aute
+        qui in ex pariatur veniam minim nulla. Officia deserunt consectetur
+        eiusmod reprehenderit laboris enim adipisicing in non. Qui sint esse
+        reprehenderit duis fugiat aute in.
+      </p>
+    </>
+  );
+}
