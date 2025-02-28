daae08a45e7d607245e6ad1da65594c4b6e7e5ed
jlsuh <joelleonardosuh@gmail.com>
2025-02-28 20:46:38 -0300

    type(ThemeContext): abstract narrower type

--- src/app/providers/ThemeContext.tsx
+++ src/app/providers/ThemeContext.tsx
@@ -4,13 +4,13 @@ import { type Context, createContext } from 'react';
 import { FALLBACK_THEME_VALUE, type ThemeValue } from '../config/theme.ts';
 
 type ThemeContextType = {
-  changeTheme: (newTheme: ThemeValue) => void;
+  changeTheme: (newTheme: string) => void;
   currentThemeValue: ThemeValue;
 };
 
 const ThemeContext: Context<ThemeContextType> = createContext<ThemeContextType>(
   {
-    changeTheme: (_: ThemeValue): void => {
+    changeTheme: (_: string): void => {
       return;
     },
     currentThemeValue: FALLBACK_THEME_VALUE,
