34f7426cc78e6a26d5c01f2930ce5a7bfc54df04
jlsuh <joelleonardosuh@gmail.com>
2025-03-02 17:23:38 -0300

    feat: wrap theme icons as children of ThemeIconContainer

--- src/shared/ui/SystemThemeIcon/SystemThemeIcon.tsx
+++ src/shared/ui/SystemThemeIcon/SystemThemeIcon.tsx
@@ -1,22 +1,23 @@
 import type { JSX } from 'react';
-import styles from './../shared.module.css';
+import ThemeIconContainer from '../ThemeIconContainer/ThemeIconContainer.tsx';
 
 function SystemThemeIcon(): JSX.Element {
   return (
-    <svg
-      className={styles.themeIconContainer}
-      fill="currentColor"
-      viewBox="0 0 16 16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>System Icon</title>
-      <path
-        clipRule="evenodd"
-        d="M7.5 2.52267C6.13332 2.64713 4.86254 3.27802 3.9372 4.29146C3.01186 5.3049 2.49882 6.62766 2.49882 8C2.49882 9.37234 3.01186 10.6951 3.9372 11.7085C4.86254 12.722 6.13332 13.3529 7.5 13.4773V2.52267ZM1.5 8C1.5 4.41 4.41 1.5 8 1.5C11.59 1.5 14.5 4.41 14.5 8C14.5 11.59 11.59 14.5 8 14.5C4.41 14.5 1.5 11.59 1.5 8Z"
+    <ThemeIconContainer>
+      <svg
         fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
+        viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title>System Icon</title>
+        <path
+          clipRule="evenodd"
+          d="M7.5 2.52267C6.13332 2.64713 4.86254 3.27802 3.9372 4.29146C3.01186 5.3049 2.49882 6.62766 2.49882 8C2.49882 9.37234 3.01186 10.6951 3.9372 11.7085C4.86254 12.722 6.13332 13.3529 7.5 13.4773V2.52267ZM1.5 8C1.5 4.41 4.41 1.5 8 1.5C11.59 1.5 14.5 4.41 14.5 8C14.5 11.59 11.59 14.5 8 14.5C4.41 14.5 1.5 11.59 1.5 8Z"
+          fill="currentColor"
+          fillRule="evenodd"
+        />
+      </svg>
+    </ThemeIconContainer>
   );
 }
 
