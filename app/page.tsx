0fcbdae78b0b60cd5a487edf090a6b860b24b784
jlsuh <joelleonardosuh@gmail.com>
2025-03-10 22:17:48 -0300

    style(Home): extract max width to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import GallerySection from '@/app/ui/GallerySection/GallerySection.tsx';
 import type { JSX } from 'react';
+import styles from './home.module.css';
 
 export default function Home(): JSX.Element {
   return (
-    <>
+    <div className={styles.home}>
       <h1>Gallery</h1>
       <GallerySection />
-    </>
+    </div>
   );
 }
