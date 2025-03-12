ede3b72d51576d45d03c0aca709d888f6ec33ca7
jlsuh <joelleonardosuh@gmail.com>
2025-03-12 01:22:26 -0300

    refactor: delete unnecessary code

--- src/app/ui/ThemeSelect/ThemeSelect.tsx
+++ src/app/ui/ThemeSelect/ThemeSelect.tsx
@@ -4,7 +4,6 @@ import { type JSX, use } from 'react';
 import { Theme } from '../../config/theme.ts';
 import ThemeContext from '../../providers/ThemeContext.tsx';
 import IconButton from '../IconButton/IconButton.tsx';
-import styles from './theme-select.module.css';
 
 function ThemeSelect(): JSX.Element {
   const { toggleTheme, currentThemeValue } = use(ThemeContext);
@@ -12,16 +11,13 @@ function ThemeSelect(): JSX.Element {
   const CurrentThemeIcon = Theme[currentThemeValue].Icon;
 
   return (
-    <div className={styles.themeSelectContainer}>
-      <IconButton
-        inputID="theme-icon-button"
-        onChangeIconButton={toggleTheme}
-        sxInput={styles.checkboxInput}
-        sxLabel={styles.button}
-      >
-        <CurrentThemeIcon />
-      </IconButton>
-    </div>
+    <IconButton
+      inputID="theme-icon-button"
+      isCheckboxControlled={true}
+      onChangeIconButton={toggleTheme}
+    >
+      <CurrentThemeIcon />
+    </IconButton>
   );
 }
 
