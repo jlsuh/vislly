d24039db08bb1890c120f4746238eadbc56681bf
jlsuh <joelleonardosuh@gmail.com>
2025-03-12 01:22:46 -0300

    refactor: button based on props

--- src/app/ui/IconButton/IconButton.tsx
+++ src/app/ui/IconButton/IconButton.tsx
@@ -1,33 +1,50 @@
 import type { JSX, PropsWithChildren } from 'react';
 import styles from './icon-button.module.css';
 
-type IconButtonProps = PropsWithChildren<{
-  inputID: string;
-  onChangeIconButton?: () => void;
-  sxInput?: string;
-  sxLabel?: string;
-}>;
+type IconButtonProps = PropsWithChildren<
+  | {
+      inputID: string;
+      isCheckboxControlled: true;
+      onChangeIconButton?: () => void;
+      sxInput?: string;
+      sxLabel?: string;
+    }
+  | {
+      inputID?: never;
+      isCheckboxControlled: false;
+      onChangeIconButton?: () => void;
+      sxInput?: never;
+      sxLabel?: never;
+    }
+>;
 
 function IconButton({
   children,
   inputID,
+  isCheckboxControlled,
   onChangeIconButton,
   sxInput = '',
   sxLabel = '',
 }: IconButtonProps): JSX.Element {
   return (
-    <label
-      className={`${styles.iconButtonContainer} ${sxLabel}`}
-      htmlFor={inputID}
-      onChange={onChangeIconButton}
-    >
-      <input
-        className={`${styles.iconButtonInput} ${sxInput}`}
-        id={inputID}
-        type="checkbox"
-      />
-      {children}
-    </label>
+    <>
+      {isCheckboxControlled ? (
+        <label
+          className={`${styles.iconButtonContainer} ${sxLabel}`}
+          htmlFor={inputID}
+          onChange={onChangeIconButton}
+        >
+          <input
+            className={`${styles.iconButtonInput} ${sxInput}`}
+            id={inputID}
+            type="checkbox"
+          />
+          {children}
+        </label>
+      ) : (
+        <div className={styles.iconButtonContainer}>{children}</div>
+      )}
+    </>
   );
 }
 
