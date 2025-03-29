c12d1f8d5f247a3a2686e9360561c77cd04f2f37
jlsuh <joelleonardosuh@gmail.com>
2025-03-28 23:13:26 -0300

    refactor: app without sidebar

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,21 +1,14 @@
 import type { JSX } from 'react';
 import BrandLogo from '../ui/BrandLogo/BrandLogo.tsx';
 import GithubIconButton from '../ui/GithubIconButton/GithubIconButton.tsx';
-import SidebarIconButton from '../ui/SidebarIconButton/SidebarIconButton.tsx';
 import ThemeSelect from '../ui/ThemeSelect/ThemeSelect.tsx';
 import styles from './header.module.css';
 
-type HeaderProps = {
-  sxHeader: string;
-  sxInput: string;
-};
-
-function Header({ sxHeader, sxInput }: HeaderProps): JSX.Element {
+function Header(): JSX.Element {
   return (
-    <header className={`${styles.header} ${sxHeader}`}>
+    <header className={styles.header}>
       <div className={styles.headerContainer}>
         <div className={styles.leftContainer}>
-          <SidebarIconButton sxInput={sxInput} />
           <BrandLogo />
         </div>
         <div className={styles.rightContainer}>
