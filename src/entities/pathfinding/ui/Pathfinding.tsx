0e140fbba5638748e6463f3a2943f3adddc8d5b3
jlsuh <joelleonardosuh@gmail.com>
2025-04-06 01:45:45 -0300

    style: extract styles

--- src/entities/pathfinding/ui/Pathfinding.tsx
+++ src/entities/pathfinding/ui/Pathfinding.tsx
@@ -4,6 +4,7 @@ import pxToRem from '@/shared/lib/pxToRem.ts';
 import useResizeDimensions from '@/shared/lib/useResizeDimensions.ts';
 import type { JSX } from 'react';
 import { useEffect, useState } from 'react';
+import styles from './pathfinding.module.css';
 
 const RESIZE_DIMENSIONS = {
   boundedHeight: 0,
@@ -15,46 +16,32 @@ const RESIZE_DIMENSIONS = {
   height: 0,
   width: 0,
 };
-const CELL_SIZE = 1;
+
+const CELL_DIM_SIZE = 1;
 
 function Pathfinding(): JSX.Element {
-  const [rows, setRows] = useState(0);
   const [cols, setCols] = useState(0);
+  const [rows, setRows] = useState(0);
   const { dimensions, ref } = useResizeDimensions(RESIZE_DIMENSIONS);
 
   useEffect(() => {
-    setRows(Math.floor(pxToRem(dimensions.boundedHeight) / CELL_SIZE));
-    setCols(Math.floor(pxToRem(dimensions.boundedWidth) / CELL_SIZE));
+    setCols(Math.floor(pxToRem(dimensions.boundedWidth) / CELL_DIM_SIZE));
+    setRows(Math.floor(pxToRem(dimensions.boundedHeight) / CELL_DIM_SIZE));
   }, [dimensions.boundedHeight, dimensions.boundedWidth]);
 
   return (
     <div
+      className={styles.grid}
       ref={ref}
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(auto-fit, ${CELL_SIZE}rem)`,
-        gridTemplateRows: `repeat(auto-fit, ${CELL_SIZE}rem)`,
-        height: '40rem',
-        justifyContent: 'center',
-        width: '100%',
-      }}
+      style={{ '--cell-size': `${CELL_DIM_SIZE}rem` }}
     >
       {Array.from({ length: rows * cols })
         .fill(0)
         .map((_, index) => {
-          const row = Math.floor(index / cols);
           const col = index % cols;
+          const row = Math.floor(index / cols);
           return (
-            <div
-              key={`cell-${row}-${col}`}
-              style={{
-                backgroundColor: 'lightgray',
-                height: `${CELL_SIZE}rem`,
-                outline: '0.0625rem solid black',
-                width: `${CELL_SIZE}rem`,
-                fontSize: '0.0325rem',
-              }}
-            />
+            <div className={styles.cell} key={`cell-row-${row}-col-${col}`} />
           );
         })}
     </div>
