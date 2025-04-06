bc8a4a03864e9ec846c336bdace9eeee3b1560eb
jlsuh <joelleonardosuh@gmail.com>
2025-04-06 01:20:41 -0300

    refactor: simplify px to rem

--- src/shared/lib/pxToRem.ts
+++ src/shared/lib/pxToRem.ts
@@ -1,9 +1,8 @@
-const pxToRem = (px: number | string): number => {
-  const tempPx = `${px}`.replace('px', '');
-  const base = window
-    .getComputedStyle(document.documentElement, null)
-    .getPropertyValue('font-size');
-  return (1 / Number.parseFloat(base)) * Number.parseInt(tempPx);
+import getRootFontSize from './getRootFontSize.ts';
+
+const pxToRem = (px: number): number => {
+  const rootFontSize = getRootFontSize();
+  return px / rootFontSize;
 };
 
 export default pxToRem;
