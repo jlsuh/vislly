8ed924aa79d9907b90abf3fec77370c613406c9e
jlsuh <joelleonardosuh@gmail.com>
2025-04-17 20:01:44 -0300

    refactor: extract nav outside of main as it is supposed to exist across the whole site

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,19 +2,21 @@ import type { JSX, PropsWithChildren } from 'react';
 import Footer from './Footer.tsx';
 import Header from './Header.tsx';
 import Main from './Main.tsx';
+import NavigationList from './NavigationList.tsx';
 import styles from './app.module.css';
 
 function App({ children }: PropsWithChildren): JSX.Element {
   return (
     <>
       <Header sxHeader={styles.header} sxInput={styles.input} />
-      <Main
-        sxContent={styles.content}
-        sxMain={styles.main}
-        sxSidebar={styles.sidebar}
-      >
-        {children}
-      </Main>
+      <div className={styles.mainContainer}>
+        <nav className={styles.sidebar}>
+          <NavigationList />
+        </nav>
+        <Main sxContent={styles.content} sxMain={styles.main}>
+          {children}
+        </Main>
+      </div>
       <Footer sxFooter={styles.footer} />
     </>
   );
--- src/app/layout/Main.tsx
+++ src/app/layout/Main.tsx
@@ -1,24 +1,14 @@
 import type { JSX, PropsWithChildren } from 'react';
-import NavigationList from './NavigationList.tsx';
 import styles from './main.module.css';
 
 type MainProps = PropsWithChildren<{
   sxContent: string;
   sxMain: string;
-  sxSidebar: string;
 }>;
 
-function Main({
-  children,
-  sxContent,
-  sxMain,
-  sxSidebar,
-}: MainProps): JSX.Element {
+function Main({ children, sxContent, sxMain }: MainProps): JSX.Element {
   return (
     <main className={`${styles.main} ${sxMain}`}>
-      <nav className={`${styles.sidebar} ${sxSidebar}`}>
-        <NavigationList />
-      </nav>
       <div className={`${styles.content} ${sxContent}`}>{children}</div>
     </main>
   );
