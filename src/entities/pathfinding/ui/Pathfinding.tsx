31b2fc5cebe24c90e009a2cca538b91e0b6d91b1
jlsuh <joelleonardosuh@gmail.com>
2025-04-17 22:47:51 -0300

    feat: make pathfinding grid respond to clicks

--- src/entities/pathfinding/ui/Pathfinding.tsx
+++ src/entities/pathfinding/ui/Pathfinding.tsx
@@ -3,9 +3,11 @@
 import composeCSSCustomProperty from '@/shared/lib/composeCSSVariable.ts';
 import pxToRem from '@/shared/lib/pxToRem.ts';
 import useResizeDimensions from '@/shared/lib/useResizeDimensions.ts';
-import type { JSX } from 'react';
-import { useEffect, useState } from 'react';
+import type { JSX, RefObject } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './pathfinding.module.css';
+
+type CellProps = { isMousePressed: RefObject<boolean> };
 
 const RESIZE_DIMENSIONS = {
   boundedHeight: 0,
@@ -24,9 +26,40 @@ const CELL_SIZE_VAR = composeCSSCustomProperty(
   `${CELL_DIM_SIZE}rem`,
 );
 
+function Cell({ isMousePressed }: CellProps): JSX.Element {
+  const [isSet, setIsSet] = useState(false);
+
+  return (
+    <button
+      className={styles.cell}
+      onMouseEnter={(): void => {
+        if (isMousePressed.current) {
+          setIsSet((prev) => !prev);
+        }
+      }}
+      onPointerDown={(): void => {
+        setIsSet((prev) => !prev);
+      }}
+      type="button"
+    >
+      <p
+        style={{
+          backgroundColor: isSet ? 'grey' : 'lightgray',
+          color: 'black',
+          fontSize: '0.5rem',
+          margin: 0,
+        }}
+      >
+        {isSet ? 1 : 0}
+      </p>
+    </button>
+  );
+}
+
 function Pathfinding(): JSX.Element {
   const [cols, setCols] = useState(0);
   const [rows, setRows] = useState(0);
+  const isHoldingClick = useRef(false);
   const { dimensions, ref } = useResizeDimensions(RESIZE_DIMENSIONS);
 
   useEffect(() => {
@@ -34,18 +67,37 @@ function Pathfinding(): JSX.Element {
     setRows(Math.floor(pxToRem(dimensions.boundedHeight) / CELL_DIM_SIZE));
   }, [dimensions.boundedHeight, dimensions.boundedWidth]);
 
+  const setIsHoldingClickToFalse = (): void => {
+    isHoldingClick.current = false;
+  };
+
+  const setIsHoldingClickToTrue = (): void => {
+    isHoldingClick.current = true;
+  };
+
   return (
-    <div className={styles.grid} ref={ref} style={CELL_SIZE_VAR}>
+    <section
+      aria-label="Pathfinding grid"
+      className={styles.grid}
+      onMouseDown={setIsHoldingClickToTrue}
+      onMouseLeave={setIsHoldingClickToFalse}
+      onMouseUp={setIsHoldingClickToFalse}
+      ref={ref}
+      style={CELL_SIZE_VAR}
+    >
       {Array.from({ length: rows * cols })
         .fill(0)
         .map((_, index) => {
           const col = index % cols;
           const row = Math.floor(index / cols);
           return (
-            <div className={styles.cell} key={`cell-row-${row}-col-${col}`} />
+            <Cell
+              isMousePressed={isHoldingClick}
+              key={`cell-row-${row}-col-${col}`}
+            />
           );
         })}
-    </div>
+    </section>
   );
 }
 
