a2f1ed081996f085e21b8c2f97be90891c073378
jlsuh <joelleonardosuh@gmail.com>
2025-07-12 23:27:09 -0300

    build: static export to gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/vislly',
   images: {
     unoptimized: true,
   },
+  output: 'export',
   reactProductionProfiling: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
