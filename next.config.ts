fe1fca9f1089b192e7456ad86742f17780d117dd
jlsuh <joelleonardosuh@gmail.com>
2025-07-12 23:14:43 -0300

    ci: delete static export

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/vislly',
   images: {
     unoptimized: true,
   },
-  output: 'export',
   reactProductionProfiling: true,
-  trailingSlash: true,
 };
 
 export default nextConfig;
