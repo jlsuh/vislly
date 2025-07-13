08056b166ede6b6a5a7546a3f7e9c41e797ccfe5
jlsuh <joelleonardosuh@gmail.com>
2025-07-13 00:16:43 -0300

    build: disable base path & trailing slash

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/vislly',
   images: {
     unoptimized: true,
   },
   output: 'export',
-  trailingSlash: false,
 };
 
 export default nextConfig;
