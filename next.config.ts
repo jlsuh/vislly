a7199d30577cb4905a797ebe50acbfa5cdd97f3e
jlsuh <joelleonardosuh@gmail.com>
2025-07-13 00:19:03 -0300

    build: enable base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/vislly',
   images: {
     unoptimized: true,
   },
   output: 'export',
+  reactProductionProfiling: true,
 };
 
 export default nextConfig;
