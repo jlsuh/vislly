1c9cae810da0de32c268a2ab0a233ab0a263a473
jlsuh <joelleonardosuh@gmail.com>
2025-07-24 22:25:01 -0300

    feat: adopt queue & exclude diagonal step if implies diagonal wall jump

--- src/entities/pathfinding/model/pathfinding-algorithms.ts
+++ src/entities/pathfinding/model/pathfinding-algorithms.ts
@@ -1,5 +1,6 @@
 import type { ReadonlyDeep } from 'type-fest';
-import type { Vertex } from './vertex.ts';
+import { Queue } from '@/shared/lib/queue.ts';
+import { START, type Vertex, WALL } from './vertex.ts';
 
 type PathfindingAlgorithm = 'bfs';
 type Step = { row: number; col: number };
@@ -19,12 +20,45 @@ const DiagonalCardinalDirections: ReadonlyDeep<Step[]> = [
 
 abstract class PathfindingStrategy {
   private isWithinBounds(
-    col: number,
     row: number,
-    gridCols: number,
+    col: number,
     gridRows: number,
+    gridCols: number,
   ): boolean {
     return row >= 0 && row < gridRows && col >= 0 && col < gridCols;
+  }
+
+  private composeOrthogonalizedDirections(diagonalStep: Step): Step[] {
+    const o1 = { row: diagonalStep.row, col: 0 };
+    const o2 = { row: 0, col: diagonalStep.col };
+    return [o1, o2];
+  }
+
+  private composeDiagonalDirectionsWithoutDiagonalWallJumps(
+    grid: Vertex[][],
+    vertex: Vertex,
+  ): Step[] {
+    const directions: Step[] = [];
+    for (const diagonalDirection of DiagonalCardinalDirections) {
+      const [o1, o2] = this.composeOrthogonalizedDirections(diagonalDirection);
+      const dRow1 = vertex.row + o1.row;
+      const dCol1 = vertex.col + o1.col;
+      if (!this.isWithinBounds(dRow1, dCol1, grid.length, grid[0].length)) {
+        continue;
+      }
+      const dRow2 = vertex.row + o2.row;
+      const dCol2 = vertex.col + o2.col;
+      if (!this.isWithinBounds(dRow2, dCol2, grid.length, grid[0].length)) {
+        continue;
+      }
+      if (
+        grid[dRow1][dCol1].name !== WALL &&
+        grid[dRow2][dCol2].name !== WALL
+      ) {
+        directions.push(diagonalDirection);
+      }
+    }
+    return directions;
   }
 
   public composeNeighbors(
@@ -34,15 +68,17 @@ abstract class PathfindingStrategy {
   ): Vertex[] {
     const directions: Step[] = [...OrthogonalCardinalDirections];
     if (isDiagonalAllowed) {
-      directions.push(...DiagonalCardinalDirections);
+      directions.push(
+        ...this.composeDiagonalDirectionsWithoutDiagonalWallJumps(grid, vertex),
+      );
     }
     const neighbors: Vertex[] = [];
     for (const dir of directions) {
       const dRow = vertex.row + dir.row;
       const dCol = vertex.col + dir.col;
-      if (this.isWithinBounds(dCol, dRow, grid[0].length, grid.length)) {
+      if (this.isWithinBounds(dRow, dCol, grid.length, grid[0].length)) {
         const neighbor = grid[dRow][dCol];
-        if (neighbor.name !== 'wall' && neighbor.name !== 'start') {
+        if (neighbor.name !== WALL && neighbor.name !== START) {
           neighbors.push(neighbor);
         }
       }
@@ -83,14 +119,14 @@ class BfsStrategy extends PathfindingStrategy {
     end: Vertex,
     isDiagonalAllowed: boolean,
   ): Generator<Vertex[], Vertex[]> {
-    const queue: Vertex[] = [start];
+    const queue: Queue<Vertex> = new Queue([start]);
     const visited: Set<Vertex> = new Set([start]);
     const previous: Map<Vertex, Vertex | null> = new Map(
       grid.flatMap((row) => row.map((vertex) => [vertex, null])),
     );
-    while (queue.length > 0) {
+    while (!queue.empty()) {
       const current =
-        queue.shift() ??
+        queue.dequeue() ??
         (() => {
           throw new Error('Queue is empty');
         })();
@@ -107,7 +143,7 @@ class BfsStrategy extends PathfindingStrategy {
         if (neighbor.name === end.name) {
           return super.reconstructPath(previous, start, neighbor);
         }
-        queue.push(neighbor);
+        queue.enqueue(neighbor);
         visited.add(neighbor);
         yield [...visited];
       }
