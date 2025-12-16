45699f01954a28045245ea452749a7a0d344565b
jlsuh <joelleonardosuh@gmail.com>
2025-12-16 19:56:46 -0300

    feat: add event loop wip

--- src/app/ui/GallerySection/GallerySection.tsx
+++ src/app/ui/GallerySection/GallerySection.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import type { JSX } from 'react';
 import brownianMotion from '@/../public/images/brownian-motion.webp';
 import pathfinding from '@/../public/images/pathfinding.webp';
+import wip from '@/../public/images/wip.webp';
 import PrefetchOnHoverLink from '@/shared/ui/PrefetchOnHoverLink/PrefetchOnHoverLink.tsx';
 import styles from './gallery-section.module.css';
 
@@ -32,6 +33,18 @@ function GallerySection(): JSX.Element {
             src={pathfinding}
           />
           <figcaption className={styles.figureCaption}>Pathfinding</figcaption>
+        </figure>
+      </PrefetchOnHoverLink>
+      <PrefetchOnHoverLink href="/event-loop">
+        <figure className={styles.figure}>
+          <Image
+            alt="Event Loop"
+            className={styles.figureImage}
+            placeholder="blur"
+            priority={true}
+            src={wip}
+          />
+          <figcaption className={styles.figureCaption}>Event Loop</figcaption>
         </figure>
       </PrefetchOnHoverLink>
     </section>
