705de2f9e1b98dd17ac9b7390786f6d7688c2c40
jlsuh <joelleonardosuh@gmail.com>
2025-12-16 22:11:59 -0300

    feat: footer with dynamic year

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -2,11 +2,13 @@ import type { JSX } from 'react';
 import styles from './footer.module.css';
 
 function Footer(): JSX.Element {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <span className={styles.copyrightNotice}>
-          Copyright &copy; 2025 Joel Suh. All rights reserved.
+          Copyright &copy; {currentYear} Joel Suh. All rights reserved.
         </span>
       </div>
     </footer>
