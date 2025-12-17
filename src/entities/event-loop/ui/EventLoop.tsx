9296a4ed984d3252d196287d7cb378599b3c3a31
jlsuh <joelleonardosuh@gmail.com>
2025-12-16 22:12:57 -0300

    feat: configure babel parser for strict JS constructs

--- src/entities/event-loop/ui/EventLoop.tsx
+++ src/entities/event-loop/ui/EventLoop.tsx
@@ -1,22 +1,30 @@
+'use client';
+
 import { parse } from '@babel/parser';
 import type { JSX } from 'react';
 
 function EventLoop(): JSX.Element {
-  const tsxCode = `
-    import type { JSX } from 'react';
-    function A(): JSX.Element {
-      return <div>Hello</div>;
-    }
-    setTimeout(() => {
-      console.log('hi');
-    });
+  const tsCode = `import { Something } from 'somewhere';
+                  function A() {
+                    return -1;
+                  }
+                  setTimeout(() => {
+                    console.log('hi');
+                  });
   `;
-  const babelAST = parse(tsxCode, {
-    sourceType: 'module',
-    plugins: ['typescript', 'jsx', 'estree'],
-    tokens: false,
-  });
-  console.log('Babel AST:\n', JSON.stringify(babelAST.program, null, 2));
+  try {
+    const babelAST = parse(tsCode, {
+      sourceType: 'module',
+      plugins: ['estree'],
+    });
+    console.log('Babel AST:\n', JSON.stringify(babelAST.program, null, 2));
+  } catch (error) {
+    console.error(
+      'Error parsing code (Strict JS Only):',
+      JSON.stringify(error, null, 2),
+    );
+  }
+
   return <></>;
 }
 
