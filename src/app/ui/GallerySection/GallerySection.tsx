864cb96d5336cc172a85b3297c99a5a4c0b45133
jlsuh <joelleonardosuh@gmail.com>
2025-12-20 01:40:02 -0300

    chore: rollback event loop

--- src/app/ui/GallerySection/GallerySection.tsx
+++ src/app/ui/GallerySection/GallerySection.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import type { JSX } from 'react';
 import brownianMotion from '@/../public/images/brownian-motion.webp';
 import pathfinding from '@/../public/images/pathfinding.webp';
-import wip from '@/../public/images/wip.webp';
 import PrefetchOnHoverLink from '@/shared/ui/PrefetchOnHoverLink/PrefetchOnHoverLink.tsx';
 import styles from './gallery-section.module.css';
 
@@ -33,18 +32,6 @@ function GallerySection(): JSX.Element {
             src={pathfinding}
           />
           <figcaption className={styles.figureCaption}>Pathfinding</figcaption>
-        </figure>
-      </PrefetchOnHoverLink>
-      <PrefetchOnHoverLink href="/event-loop">
-        <figure className={styles.figure}>
-          <Image
-            alt="Event Loop"
-            className={styles.figureImage}
-            placeholder="blur"
-            priority={true}
-            src={wip}
-          />
-          <figcaption className={styles.figureCaption}>Event Loop</figcaption>
         </figure>
       </PrefetchOnHoverLink>
     </section>
