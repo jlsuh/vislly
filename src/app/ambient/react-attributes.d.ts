36319de19caaa1c71b555f5a73a67fa9d2677774
jlsuh <joelleonardosuh@gmail.com>
2026-01-05 21:06:24 -0300

    types: extend command attributes to ButtonHTMLAttributes

---
+++ src/app/ambient/react-attributes.d.ts
@@ -0,0 +1,26 @@
+import 'react';
+
+declare module 'react' {
+  /**
+   * @see https://github.com/DefinitelyTyped/DefinitelyTyped/pull/73957
+   */
+  interface ButtonHTMLAttributes<T> extends HTMLAttributes<T> {
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement/command}
+     */
+    command?:
+      | 'show-modal'
+      | 'close'
+      | 'request-close'
+      | 'show-popover'
+      | 'hide-popover'
+      | 'toggle-popover'
+      | `--${string}`
+      | undefined;
+
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement/commandFor}
+     */
+    commandfor?: string | undefined;
+  }
+}
