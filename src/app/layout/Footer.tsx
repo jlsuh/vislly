d4cb15dd8c65ce70fde736bbfaec2cf7382e3cf7
jlsuh <joelleonardosuh@gmail.com>
2026-01-23 20:41:12 -0300

    feat: last deploy date

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -3,13 +3,19 @@ import styles from './footer.module.css';
 
 function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear();
+  const lastDeployDate = process.env.NEXT_PUBLIC_LAST_DEPLOY_DATE;
 
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <span className={styles.copyrightNotice}>
-          Copyright &copy; {currentYear} Joel Suh. All rights reserved.
+          Copyright &copy; {currentYear} Joel Suh
         </span>
+        {lastDeployDate && (
+          <span className={styles.deployNotice}>
+            Last deploy: {lastDeployDate}
+          </span>
+        )}
       </div>
     </footer>
   );
