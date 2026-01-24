c8504ed4a88b5d078f6e17fb1b8d1a48267438d3
jlsuh <joelleonardosuh@gmail.com>
2026-01-23 21:46:43 -0300

    build: last deploy date

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   reactProductionProfiling: true,
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_LAST_DEPLOY_DATE: new Date().toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+    }),
+  },
 };
 
 export default nextConfig;
